cfe2cd417f9d3172248dd8d91b19364059a14fc8
Dumb00heavycoder <dhruvlgd44@gmail.com>
2026-04-10 00:58:33 +0530

    rolling back to earlier version of dashboard

--- buildezy/dashboard/urls.py
+++ buildezy/dashboard/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 
-urlpatterns = [ 
-               path('', view.dashboard_view, name='dashboard'),
-               ]
+urlpatterns = [
+    path('', views.dashboard_view, name='dashboard'),
+]
